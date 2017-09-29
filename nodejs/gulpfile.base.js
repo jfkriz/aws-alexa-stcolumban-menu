1ba5b9d87a3ea5a3e7195cd23a62509b63da9c3d
Jim Kriz <jim@kriz.net>
2017-09-29 16:21:20 -0400

    Fixes for codeship

--- nodejs/gulpfile.base.js
+++ nodejs/gulpfile.base.js
@@ -5,12 +5,12 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 
 module.exports = {
-    lint : () => {
+    lint : function() {
         return gulp.src(['lib/**/*.js', 'test/**/*.js'])
                .pipe(jshint())
                .pipe(jshint.reporter('default'));
     },
-    test : () => {
+    test : function() {
         return gulp.src('test/**/*.spec.js', { read : false })
             .pipe(mocha({ reporter : 'nyan' }));
     }
