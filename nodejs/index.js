a7eb7bddd1df4c1a24a8d48fb9c791b330bb4a99
Jim Kriz <jim@kriz.net>
2017-09-29 14:12:14 -0400

    Finally working

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,9 +2,18 @@ var Alexa = require('alexa-sdk');
 var lib = require('./lib');
 var appId = process.env.AlexaSkillId;
 
+var helpHandlers = {
+    'LaunchRequest': () => {
+        this.emit('AMAZON.HelpIntent');
+    },
+    'AMAZON.HelpIntent': () => {
+        this.emit(':tell', 'Sorry, you need to say something like "Alexa, ask Saint Columban what\'s on the lunch menu today" or "Alexa, ask Saint Columban what\'s for lunch next Friday".');
+    }
+}
+
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context, callback);
     alexa.appId = appId;
-    alexa.registerHandlers(lib.Skills.DailyMenu.Handlers, lib.Skills.BibleVerse.Handlers);
+    alexa.registerHandlers(lib.Skills.DailyMenu.Handlers, lib.Skills.BibleVerse.Handlers, helpHandlers);
     alexa.execute();
 };
